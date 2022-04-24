b79f9fbe65884f6b7efcf1d0f803a5be1f787851
wuseokjung <wuseok.jung@gmail.com>
2022-04-24 00:28:56 -0400

    spacer component added

--- src/features/restaurants/components/infoCard.js
+++ src/features/restaurants/components/infoCard.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { View, Text, Image } from 'react-native';
 import styled from 'styled-components/native';
 import { Card } from 'react-native-paper';
 import { SvgXml } from 'react-native-svg';
+import { Spacer } from '../../../components/spacer/spacer.component';
 import star from '../../../../assets/star';
+import open from '../../../../assets/open';
 
 //Kind of like CSS
 const Title = styled.Text`
@@ -32,18 +35,26 @@ const Rating = styled.View`
 	padding-top: ${(props) => props.theme.space[2]};
 	padding-bottom: ${(props) => props.theme.space[2]};
 `;
-
+const Section = styled.View`
+	flex-direction: row;
+	align-items: center;
+`;
+const SectionEnd = styled.View`
+	flex: 1;
+	flex-direction: row;
+	justify-content: flex-end;
+`;
 export const RestaurantInfo = ({ restaurant = {} }) => {
 	const {
 		name = 'Bulla Gas',
-		icon,
+		icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
 		photos = [
 			'https://cdn.vox-cdn.com/thumbor/2bH4wLHTPbxbZT0YdrH5hxxWORw=/0x0:960x640/1400x1400/filters:focal(404x244:556x396):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/55753635/13178539_952627198190586_738313161884253238_n.0.0.jpg',
 		],
 		address = '8525 Native Pine Way',
 		isOpenNow = true,
 		rating = 4,
-		isClosedTemporarily,
+		isClosedTemporarily = true,
 	} = restaurant;
 
 	const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -53,11 +64,24 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
 			<CardCover key={name} source={{ uri: photos[0] }} />
 			<Info>
 				<Title>{name}</Title>
-				<Rating>
-					{ratingArray.map(() => (
-						<SvgXml xml={star} width={20} height={20} />
-					))}
-				</Rating>
+				<Section>
+					<Rating>
+						{ratingArray.map(() => (
+							<SvgXml xml={star} width={20} height={20} />
+						))}
+					</Rating>
+					<SectionEnd>
+						{isClosedTemporarily && (
+							<Text variant="label" style={{ color: 'red' }}>
+								CLOSED TEMPORARILY
+							</Text>
+						)}
+						<Spacer variant="left.large" />
+						{isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+						<Spacer variant="left.large" />
+						<Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+					</SectionEnd>
+				</Section>
 				<Address>{address}</Address>
 			</Info>
 		</ResCard>
